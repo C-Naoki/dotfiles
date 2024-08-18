69a213d6df156eb3a357140f4df423b8fb33bd4a
naoki <u429561d@ecs.osaka-u.ac.jp>
2024-08-19 07:37:35 +0900

    🎨 style: standardize tab size to 4 spaces

--- scripts/symlink.sh
+++ scripts/symlink.sh
@@ -6,29 +6,29 @@ DOTDIR=${DOTDIR:="$HOME"/.dotfiles/macOS}
 
 # config
 for file in "$DOTDIR"/config/*/.[^.]*; do
-  # ignore .gitkeep
-  if echo "$file" | grep -q '.gitkeep'; then
-    continue
-  fi
-  # make linkname
-  linkname=~/$(basename "$file")
-  # create symlink
-  ln -sfn "$file" "$linkname"
-  # output
-  echo "create symlink: '$file' -> '$linkname'"
+    # ignore .gitkeep
+    if echo "$file" | grep -q '.gitkeep'; then
+        continue
+    fi
+    # make linkname
+    linkname=~/$(basename "$file")
+    # create symlink
+    ln -sfn "$file" "$linkname"
+    # output
+    echo "create symlink: '$file' -> '$linkname'"
 done
 
 # history
 for file in "$DOTDIR"/local/state/*/history; do
-  # ignore .gitkeep
-  if echo "$file" | grep -q '.gitkeep'; then
-    continue
-  fi
-  # make linkname
-  tmp="${file%/*}"
-  linkname=~/."${tmp##*/}"_history
-  # create symlink
-  ln -sfn "$file" "$linkname"
-  # output
-  echo "create symlink: '$file' -> '$linkname'"
+    # ignore .gitkeep
+    if echo "$file" | grep -q '.gitkeep'; then
+        continue
+    fi
+    # make linkname
+    tmp="${file%/*}"
+    linkname=~/."${tmp##*/}"_history
+    # create symlink
+    ln -sfn "$file" "$linkname"
+    # output
+    echo "create symlink: '$file' -> '$linkname'"
 done
--- scripts/unlink.sh
+++ scripts/unlink.sh
@@ -6,33 +6,33 @@ DOTDIR=${DOTDIR:="$HOME"/.dotfiles/macOS}
 
 # config
 for file in "$DOTDIR"/config/*/.[^.]*; do
-  # ignore .gitkeep
-  if echo "$file" | grep -q '.gitkeep'; then
-    continue
-  fi
-  # make linkname
-  linkname=~/$(basename "$file")
-  # delete symlink
-  if [ -L "$linkname" ]; then
-    unlink "$linkname"
-  fi
-  # output
-  echo "delete symlink: '$linkname'"
+    # ignore .gitkeep
+    if echo "$file" | grep -q '.gitkeep'; then
+        continue
+    fi
+    # make linkname
+    linkname=~/$(basename "$file")
+    # delete symlink
+    if [ -L "$linkname" ]; then
+        unlink "$linkname"
+    fi
+    # output
+    echo "delete symlink: '$linkname'"
 done
 
 # history
 for file in "$DOTDIR"/local/state/*/history; do
-  # ignore .gitkeep
-  if echo "$file" | grep -q '.gitkeep'; then
-    continue
-  fi
-  # make linkname
-  tmp="${file%/*}"
-  linkname=~/."${tmp##*/}"_history
-  # delete symlink
-  if [ -L "$linkname" ]; then
-    unlink "$linkname"
-  fi
-  # output
-  echo "delete symlink: '$linkname'"
+    # ignore .gitkeep
+    if echo "$file" | grep -q '.gitkeep'; then
+        continue
+    fi
+    # make linkname
+    tmp="${file%/*}"
+    linkname=~/."${tmp##*/}"_history
+    # delete symlink
+    if [ -L "$linkname" ]; then
+        unlink "$linkname"
+    fi
+    # output
+    echo "delete symlink: '$linkname'"
 done
