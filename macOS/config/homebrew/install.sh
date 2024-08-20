49be62421e24a599f42ffb653c09a5a5de62684d
naoki <u429561d@ecs.osaka-u.ac.jp>
2024-08-20 22:26:00 +0900

    ✨ feat: add a new shell file for installing Homebrew

---
+++ macOS/config/homebrew/install.sh
@@ -0,0 +1,10 @@
+# Install Homebrew
+if ! type brew > /dev/null 2>&1; then
+    echo "Install Homebrew..."
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+else
+    echo "Homebrew has already been installed."
+fi
+
+# Install Homebrew packages all at once
+brew bundle -v --file=.Brewfile
