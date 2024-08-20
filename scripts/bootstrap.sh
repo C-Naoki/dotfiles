f5b283e96fb3e192f14f8e156351d886fb5650ea
naoki <u429561d@ecs.osaka-u.ac.jp>
2024-08-20 23:05:26 +0900

    🐛 fix: correct the path in bootstrap.sh

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -11,18 +11,6 @@ echo "🚀 Starting the bootstrap process..."
 # dotfiles directory
 export DOTDIR="$HOME/.dotfiles"
 
-# check if dotfiles exists
-if [ ! -d ${DOTDIR} ]; then
-    # check your PC's OS
-    if [ $(uname) == "Darwin" ]; then
-        DOTDIR="$DOTDIR/macOS"
-    elif [ $(uname) == "Linux" ]; then
-        DOTDIR="$DOTDIR/linux"
-    else
-        echo "Your PC's OS is not supported"
-        exit 1
-    fi
-
     # download dotfiles and move to the dotfiles directory
     if has "git"; then
         git clone https://github.com/C-Naoki/dotfiles.git ${DOTDIR}
@@ -41,13 +29,27 @@ if [ ! -d ${DOTDIR} ]; then
         exit 1
     fi
 
+# check if dotfiles exists
+if [ ! -d ${DOTDIR} ]; then
+    # check your PC's OS
+    if [ $(uname) == "Darwin" ]; then
+        DOTDIR="$DOTDIR/macOS"
+    elif [ $(uname) == "Linux" ]; then
+        DOTDIR="$DOTDIR/linux"
+    else
+        echo "Your PC's OS is not supported"
+        exit 1
+    fi
+
     # move to the dotfiles directory
     cd ${DOTDIR}
 
-    # set up dotfiles
-    sh $DOTDIR/scripts/install-brew.sh
-    sh $DOTDIR/scripts/set-history.sh
-    sh $DOTDIR/scripts/symlink.sh
+    if [ $(uname) == "Darwin" ]; then
+        # set up dotfiles
+        sh ../scripts/install-brew.sh
+        sh ../scripts/set-history.sh
+        sh ../scripts/symlink.sh
+    fi
 else
     echo "dotfiles already exists"
     exit 1
